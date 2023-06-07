1d2bc8726ccf2d9f21446560c905c19892bcfc01
Do Trong Thu <lmt42@yandex.com>
2023-06-07 15:23:37 +0700

    Add dead pop particle

--- Assets/Scripts/DamagableController.cs
+++ Assets/Scripts/DamagableController.cs
@@ -6,10 +6,16 @@ namespace CountMasterClone
 {
     public class DamagableController : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject deadPopParticle;
+
         public event Action<GameObject, HitReason> Died;
 
         public void Hit(HitReason reason)
         {
+            GameObject popParticleObj = Instantiate(deadPopParticle, null, false);
+            popParticleObj.transform.position += transform.position;
+
             Destroy(gameObject);
             Died?.Invoke(gameObject, reason);
         }
